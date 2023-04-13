8c392b57787c87b2b4ce7804ff2833dcbdf56dd7
KingRainbow44 <kobedo11@gmail.com>
2023-04-13 17:37:32 -0400

    Auto-enable `questing` in the config

--- src/main/java/emu/grasscutter/config/ConfigContainer.java
+++ src/main/java/emu/grasscutter/config/ConfigContainer.java
@@ -235,9 +235,9 @@ public class ConfigContainer {
         public boolean staminaUsage = true;
         public boolean energyUsage = true;
         public boolean fishhookTeleport = true;
+        public boolean questing = true;
         public ResinOptions resinOptions = new ResinOptions();
         public Rates rates = new Rates();
-        public boolean questing = false;
 
         public static class InventoryLimits {
             public int weapons = 2000;
