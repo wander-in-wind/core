49a3a16ca3e2596c7354e3d26a9086b34d1649f9
hartie95 <mail@hartie95.de>
2023-08-07 21:12:48 +0200

    [Fix] Sync EntityIdType and EntityType for types used by scripts

--- src/main/java/emu/grasscutter/game/props/EntityIdType.java
+++ src/main/java/emu/grasscutter/game/props/EntityIdType.java
@@ -1,15 +1,18 @@
 package emu.grasscutter.game.props;
 
+/**
+ * TODO for now keep it in sync with EntityType, maybe replace this by EntityType
+ */
 public enum EntityIdType {
 	NONE(0x00),
 	AVATAR	(0x01),
 	MONSTER	(0x02),
-	NPC		(0x03),
-	GADGET	(0x04),
+	NPC		(0x0C),
+	GADGET	(0x13),
     REGION	(0x05),
-    WEAPON	(0x06),
-	TEAM 	(0x09),
-	MPLEVEL	(0x0b);
+    WEAPON	(0x08),
+	TEAM 	(0x1F),
+	MPLEVEL	(0x2A);
 
 	private final int id;
 
