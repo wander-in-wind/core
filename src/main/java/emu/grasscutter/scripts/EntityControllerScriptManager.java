scooterboo/Grasscutter-Quests

[Logging] Added added extra logger configs for more granulare loglevers This mostly affects bigger GC modules like Scripting, abilities and resource loading, with questing following

src/main/java/emu/grasscutter/scripts/EntityControllerScriptManager.java
@@ -1,11 +1,10 @@
 package emu.grasscutter.scripts;
 
-import emu.grasscutter.Grasscutter;
+import emu.grasscutter.Loggers;
 import emu.grasscutter.scripts.data.controller.EntityController;
 import lombok.val;
+import org.slf4j.Logger;
 
-import javax.script.Bindings;
-import javax.script.CompiledScript;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Map;
@@ -14,6 +13,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import static emu.grasscutter.utils.FileUtils.getScriptPath;
 
 public class EntityControllerScriptManager {
+    private static final Logger logger = Loggers.getScriptSystem();
     private static final Map<String, EntityController> gadgetController = new ConcurrentHashMap<>();
 
     public static void load(){
@@ -25,23 +25,25 @@ public class EntityControllerScriptManager {
             stream.forEach(path -> {
                 val fileName = path.getFileName().toString();
 
-                if(!fileName.endsWith(".lua")) return;
+                if(!fileName.endsWith(".lua"))
+                    return;
 
                 val controllerName = fileName.substring(0, fileName.length()-4);
                 val cs = ScriptLoader.getScript("Gadget/"+fileName);
-                if (cs == null) return;
+                if (cs == null)
+                    return;
 
                 try{
                     cs.evaluate();
                     gadgetController.put(controllerName, new EntityController(cs));
                 } catch (Throwable e){
-                    Grasscutter.getLogger().error("Error while loading gadget controller: {}", fileName);
+                    logger.error("Error while loading gadget controller: {}", fileName, e);
                 }
             });
 
-            Grasscutter.getLogger().info("Loaded {} gadget controllers", gadgetController.size());
+            logger.info("Loaded {} gadget controllers", gadgetController.size());
         } catch (IOException e) {
-            Grasscutter.getLogger().error("Error loading gadget controller luas");
+            logger.error("Error loading gadget controller luas");
         }
     }
 

wander-in-wind/core

src/main/java/emu/grasscutter/scripts/EntityControllerScriptManager.java
@@ -1,11 +1,10 @@
 package emu.grasscutter.scripts;
 
-import emu.grasscutter.Grasscutter;
+import emu.grasscutter.Loggers;
 import emu.grasscutter.scripts.data.controller.EntityController;
 import lombok.val;
+import org.slf4j.Logger;
 
-import javax.script.Bindings;
-import javax.script.CompiledScript;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Map;
@@ -14,6 +13,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import static emu.grasscutter.utils.FileUtils.getScriptPath;
 
 public class EntityControllerScriptManager {
+    private static final Logger logger = Loggers.getScriptSystem();
     private static final Map<String, EntityController> gadgetController = new ConcurrentHashMap<>();
 
     public static void load(){
@@ -24,23 +24,25 @@ public class EntityControllerScriptManager {
         try (val stream = Files.newDirectoryStream(getScriptPath("Gadget/"), "*.lua")) {
             stream.forEach(path -> {
                 val fileName = path.getFileName().toString();
-                if (!fileName.endsWith(".lua")) return;
+                if (!fileName.endsWith(".lua"))
+                    return;
 
                 val controllerName = fileName.substring(0, fileName.length()-4);
                 val cs = ScriptLoader.getScript("Gadget/"+fileName);
-                if (cs == null) return;
+                if (cs == null)
+                    return;
 
                 try {
                     cs.evaluate();
                     gadgetController.put(controllerName, new EntityController(cs));
                 } catch (Throwable e) {
-                    Grasscutter.getLogger()
-                        .error("Error while loading gadget controller: {}.", fileName);
+                    logger
+                        .error("Error while loading gadget controller: {}.", fileName, e);
                 }
             });
-            Grasscutter.getLogger().debug("Loaded {} gadget controllers", gadgetController.size());
+            logger.debug("Loaded {} gadget controllers", gadgetController.size());
         } catch (IOException e) {
-            Grasscutter.getLogger().error("Error loading gadget controller Lua scripts.");
+            logger.error("Error loading gadget controller Lua scripts.");
         }
     }
 